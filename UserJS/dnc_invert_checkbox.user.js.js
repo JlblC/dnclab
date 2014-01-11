1c79deb17ef930b98f2d83a0d1c629dd2611e213
JlblC <jlblc@ya.ru>
2014-01-11 17:58:48 +0400

    ver 0.1

--- UserJS/dnc_invert_checkbox.user.js.js
+++ UserJS/dnc_invert_checkbox.user.js.js
@@ -1,10 +1,11 @@
 // ==UserScript==
-// @name       dnc_invert_checkbox
+// @name       DnC invert checkboxes
 // @namespace  http://use.i.E.your.homepage/
 // @version    0.1
 // @description  enter something useful
+// @updateURL    https://raw2.github.com/JlblC/dnclab/master/UserJS/dnc_invert_checkbox.user.js.js
+// @downloadURL  https://raw2.github.com/JlblC/dnclab/master/UserJS/dnc_invert_checkbox.user.js.js
 // @match      http://*.the-game.ru/*
-// @copyright  2012+, You
 // ==/UserScript==
 
 if (document.querySelectorAll('input[type="checkbox"]')>9) {
@@ -14,6 +15,6 @@ if (document.querySelectorAll('input[type="checkbox"]')>9) {
     		var cb=document.querySelectorAll('input[type="checkbox"]');
         	for (var i=0; i<cb.length; i++) cb[i].checked=!cb[i].checked;
     		};
-    	b.value=' ☑<->☐';
+    	b.value=' ☑<–>☐';
     	document.body.appendChild(b);    
 	}
