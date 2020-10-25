0c4e610300580a9f0448290aabed6d0c46707f0b
JlblC <jlblc@ya.ru>
2020-10-25 17:30:02 +0400

    Update monster.user.js

--- UserJS/monster.user.js
+++ UserJS/monster.user.js
@@ -4,14 +4,28 @@
 // @version      0.1.1
 // @description  try to take over the world!
 // @author       You
-// @match        *://the-game.ru/frames/planet_fleets/on/*
-// @match        *://*.the-game.ru/frames/planet_fleets/on/*
+// @match        *://the-game.ru/planet/*
+// @match        *://*.the-game.ru/planet/*
 // @updateURL    https://github.com/JlblC/dnclab/raw/master/UserJS/monster.user.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
+    $=document.querySelector;
+    const 
+        pf = document.querySelector('#planet_fleets'),
+        td = pf.parentElement,
+        ht = pf.contentDocument.documentElement,
+        bd = ht.lastElementChild;
+    let i=0;
+    for (let z of [pf,td,ht,bd]) {
+        //z.style.backgroundColor=['red','blue','green','yelloy'][i++];
+        z.style.width="100%";
+    }
+          
+          
+                 
     function monster(){
         const a=['🐉','👾','🐙','👽'];
         let m=document.createElement('div');
@@ -20,6 +34,5 @@
         document.querySelector('.monster')?.remove();
         document.body.append(m);
     }
-    setInterval(monster,500);
-    // Your code here...
+    
 })();
